e3440491b7d0a86d914c564a59dfbfd8c847aa9a
M4916297 <108253931+M4916297@users.noreply.github.com>
2024-08-06 17:07:54 +0800

    Add files via upload

---
+++ Qt/widget.h
@@ -0,0 +1,45 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QList>
+#include <settings.h>
+#include <login.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+public slots:
+
+
+private slots:
+    void on_set_btn_clicked();
+
+private:
+    Ui::Widget *ui;
+
+
+    //串口ui设置界面
+    Settings *settingsUi;
+
+    //登录ui界面
+    Login *loginui;
+
+    //ui界面内容初始化
+    void Init();
+
+
+
+};
+#endif // WIDGET_H
